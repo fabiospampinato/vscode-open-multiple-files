6d4e8e32d00e85ba1016491d21efcff20c92b136
al <usernamehw@gmail.com>
2018-12-16 15:39:39 +0300

    Remove padding for import

--- src/commands.ts
+++ src/commands.ts
@@ -8,7 +8,7 @@ import * as pify from 'pify';
 import * as vscode from 'vscode';
 import Config from './config';
 import Utils from './utils';
-import { IContextMenuPath } from './types';
+import {IContextMenuPath} from './types';
 
 
 /* COMMANDS */
